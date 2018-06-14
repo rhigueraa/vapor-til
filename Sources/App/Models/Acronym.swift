71239e92dbe9362043590beb961a96cb4d6860a3
Rodrigo Higuera <rhigueraa@gmail.com>
2018-06-14 18:17:34 -0500

    added put and get routes

--- Sources/App/Models/Acronym.swift
+++ Sources/App/Models/Acronym.swift
@@ -5,6 +5,7 @@ final class Acronym: Codable {
     var id: Int?
     var short: String
     var long: String
+    
     init(short: String, long: String) {
         self.short = short
         self.long = long
@@ -14,3 +15,4 @@ final class Acronym: Codable {
 extension Acronym: PostgreSQLModel {}
 extension Acronym: Migration {}
 extension Acronym: Content {}
+extension Acronym: Parameter {}
