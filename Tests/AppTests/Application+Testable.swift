2ff873e007ce86c1dc0b9d9d07c7d3ec42478d96
Rodrigo Higuera <rhigueraa@gmail.com>
2018-06-25 22:13:21 -0500

    reverted testing env

--- Tests/AppTests/Application+Testable.swift
+++ Tests/AppTests/Application+Testable.swift
@@ -7,7 +7,7 @@ extension Application {
     static func testable(envArgs: [String]? = nil) throws -> Application {
         var config = Config.default()
         var services = Services.default()
-        var env = Environment.production
+        var env = Environment.testing
         if let environmentArgs = envArgs {
             env.arguments = environmentArgs
         }
